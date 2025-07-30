ae880c3d972691fdcbb5fb242f9b069ff4f0c2e8
Tatsumasa651 <tatsumasa1016@gmail.com>
2025-07-30 16:18:55 +0900

    9.4km

--- src/web/config.js
+++ src/web/config.js
@@ -1,5 +1,12 @@
+// config.js
+const hostname = window.location.hostname;
+const isDev = ['localhost', '127.0.0.1'].includes(hostname); // 開発環境判定（localhost または 127.0.0.1）
+
 const config = {
-  apiUrl: '/api_tatsumasa_nakano'
+  apiUrl: isDev
+    ? 'http://localhost:3000/api_tatsumasa_nakano'
+    : 'http://dev.marathon.rplearn.net/api_tatsumasa_nakano'
 };
 
 export default config;
+
